59bc0a6d1054e7d26922de93ddc51eb2923e667d
londoncute <107755910+londoncute@users.noreply.github.com>
2025-04-27 23:02:41 +0300

    Update UnitTest1.cs

--- fa.Tests/UnitTest1.cs
+++ fa.Tests/UnitTest1.cs
@@ -1,98 +1,64 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using fans;
-namespace NET
+using System;
+
+namespace UnitTests
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestFA1()
         {
-            String s = "0111";
-            FA1 fa = new FA1();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == true);
+            FA1 fa1 = new FA1();
+            
+            // Should accept: exactly one 0 and at least one 1
+            Assert.IsTrue(fa1.Run("01"));
+            Assert.IsTrue(fa1.Run("011"));
+            Assert.IsTrue(fa1.Run("101"));
+            
+            // Should reject
+            Assert.IsFalse(fa1.Run("0"));     // no 1s
+            Assert.IsFalse(fa1.Run("1"));     // no 0s
+            Assert.IsFalse(fa1.Run("001"));   // two 0s
+            Assert.IsFalse(fa1.Run("11"));    // no 0s
+            Assert.IsNull(fa1.Run("01a"));    // invalid character
         }
+
         [TestMethod]
-        public void TestMethod2()
+        public void TestFA2()
         {
-            String s = "01011";
-            FA1 fa = new FA1();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == false);
-        }   
-        [TestMethod]
-        public void TestMethod3()
-        {
-            String s = "110101011";
-            FA1 fa = new FA1();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == false);
-        }  
-        [TestMethod]
-        public void TestMethod4()
-        {
-            String s = "1110111";
-            FA1 fa = new FA1();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == true);
-        }  
-        [TestMethod]
-        public void TestMethod5()
-        {
-            String s = "10";
-            FA1 fa = new FA1();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == true);
-        } 
-        [TestMethod]
-        public void TestMethod6()
-        {
-            String s = "0101";
-            FA2 fa = new FA2();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == false);
-        }  
-        [TestMethod]
-        public void TestMethod7()
-        {
-            String s = "00110011";
-            FA2 fa = new FA2();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == false);
-        }  
-        [TestMethod]
-        public void TestMethod8()
-        {
-            String s = "0001";
-            FA2 fa = new FA2();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == true);
-        }  
-        [TestMethod]
-        public void TestMethod9()
-        {
-            String s = "111000";
-            FA2 fa = new FA2();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == true);
-        } 
-        [TestMethod]
-        public void TestMethod10()
-        {
-            String s = "00110011";
-            FA3 fa = new FA3();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == true);
+            FA2 fa2 = new FA2();
+            
+            // Should accept: odd number of 0s and odd number of 1s
+            Assert.IsTrue(fa2.Run("01"));     // 1 '0', 1 '1' (odd, odd)
+            Assert.IsFalse(fa2.Run("00111")); // 2 '0's, 3 '1's (even, odd)
+            Assert.IsFalse(fa2.Run("101"));   // 1 '0', 2 '1's (odd, even)
+            
+            // Should reject
+            Assert.IsFalse(fa2.Run("00"));    // even 0s, even 1s
+            Assert.IsFalse(fa2.Run("11"));    // even 0s, even 1s
+            Assert.IsFalse(fa2.Run("001"));   // odd 0s, even 1s
+            Assert.IsFalse(fa2.Run("011"));   // even 0s, odd 1s
+            Assert.IsNull(fa2.Run("01a"));    // invalid character
         }
+
         [TestMethod]
-        public void TestMethod11()
+        public void TestFA3()
         {
-            String s = "0101";
-            FA3 fa = new FA3();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == false);
-        }     
+            FA3 fa3 = new FA3();
+            
+            // Should accept: contains '11'
+            Assert.IsTrue(fa3.Run("11"));
+            Assert.IsTrue(fa3.Run("011"));
+            Assert.IsTrue(fa3.Run("0011"));
+            
+            // Should reject
+            Assert.IsFalse(fa3.Run("0"));
+            Assert.IsFalse(fa3.Run("1"));
+            Assert.IsFalse(fa3.Run("101"));
+            Assert.IsFalse(fa3.Run("010"));
+            Assert.IsNull(fa3.Run("11a"));    // invalid character
+        }
     }
 }
